97a9fec20ebbf5e83d61e7947a76cf85ae22d328
Huan Ning <gladcolor@gmail.com>
2023-06-16 23:09:55 -0400

    first commit

--- Tweet_Geo_kernel.py
+++ Tweet_Geo_kernel.py
@@ -1,10 +1,9 @@
-import Tweet_Geo_Constants as constants
+import Tweet_Geo_constants as constants
 import helper
 import os
 import requests
-import networkx as nx
 import pandas as pd
-import geopandas as gpd
+# import geopandas as gpd
 # from pyvis.network import Network
 import openai
 import pickle
@@ -13,36 +12,79 @@ import sys
 import traceback
 
 
-class Tweet():
-    """
-    """
+
+class Localization():
     
-    pass
+    def __init__(self,
+        
+                user_df,
+                model='gpt-3.5-turbo-0613',
+                max_tweet=100,
+                used_cols=['description', 'location']
+                  
+                ):
+        assert len(user_df) <= max_tweet, f"The tweet count is {len(user_df)}; it should <= {max_tweet}!"
+        self.user_df = user_df
+        self.used_cols = used_cols
+        self.prompt_for_user_localization = self.get_prompt_for_user_localization()
+        
+        self.use_loc_LLM_response = ""
+        self.model = model
+ 
+     
+    def get_prompt_for_user_localization(self):        
+        user_loc_requirement_str = '\n'.join([f"{idx + 1}. {line}" for idx, line in enumerate(constants.user_loc_requirements)])
+        list_of_dict = self.user_df[self.used_cols].to_dict('records')
+
+        user_loc_prompt = f"Your role: {constants.user_loc_role} \n" + \
+                          f"Your task: {constants.user_loc_task_prefix} \n" + \
+                          f"Requirement:\n{user_loc_requirement_str} \n" + \
+                          f"List of user discription and self-report home location:\n{list_of_dict}"
+        
+        return user_loc_prompt
+        
+        # self.prompt_for_user_localization = user_loc_prompt
+
+    def get_LLM_user_loc_response(self):
+        user_loc_LLM_response = helper.get_LLM_reply(
+                          prompt=self.prompt_for_user_localization,
+                          system_role=constants.user_loc_role,
+                          model=self.model,
+                          # model=r"gpt-4",
+                         )
+        self.use_loc_LLM_response = user_loc_LLM_response
+          
+        return self.use_loc_LLM_response
+# class Tweet():
+#     """
+#     """
+    
+#     pass
     
     
     
-class User():
-    """
-    """
+# class User():
+#     """
+#     """
     
-    pass
+#     pass
 
 
-class Media():
-    """
-    """
+# class Media():
+#     """
+#     """
     
-    pass
+#     pass
 
 
-class Place():
-    """
-    """
+# class Place():
+#     """
+#     """
     
-    pass
+#     pass
 
-class Poll():
-    """
-    """
+# class Poll():
+#     """
+#     """
     
-    pass
+#     pass
--- helper.py
+++ helper.py
@@ -15,3 +15,74 @@ from pyvis.network import Network
  
 
 import Tweet_Geo_constants as constants
+
+def clean_tweet(tweet_text):
+    # Remove URLs
+    tweet_text = re.sub(r'http\S+|www.\S+', '', tweet_text, flags=re.MULTILINE)
+    # Remove usernames/mentions
+    
+    tweet_text = re.sub(r'@\w+\s+', '', tweet_text)
+    tweet_text = re.sub(r'@\w+[ ,]*', '', tweet_text)
+    return tweet_text
+
+    # Example usage:
+    # tweet = "@user This is a tweet with a URL: http://example.com"
+    # print(clean_tweet_text(tweet))
+    
+    
+    
+def get_LLM_reply(prompt="Provide Python code to read a CSV file from this URL and store the content in a variable. ",
+                  system_role=r'You are a professional Geo-information scientist and developer.',
+                  model=r"gpt-3.5-0613",
+                  verbose=True,
+                  temperature=1,
+                  stream=True,
+                  retry_cnt=3,
+                  sleep_sec=10,
+                  ):
+    openai.api_key = constants.OpenAI_key
+
+    # Generate prompt for ChatGPT
+    # url = "https://github.com/gladcolor/LLM-Geo/raw/master/overlay_analysis/NC_tract_population.csv"
+    # prompt = prompt + url
+
+    # Query ChatGPT with the prompt
+    # if verbose:
+    #     print("Geting LLM reply... \n")
+    count = 0
+    isSucceed = False
+    while (not isSucceed) and (count < retry_cnt):
+        try:
+            count += 1
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                {"role": "system", "content": system_role},
+                {"role": "user", "content": prompt},
+                ],
+                temperature=temperature,
+                stream=stream,
+                )
+        except Exception as e:
+            # logging.error(f"Error in get_LLM_reply(), will sleep {sleep_sec} seconds, then retry {count}/{retry_cnt}: \n", e)
+            print(f"Error in get_LLM_reply(), will sleep {sleep_sec} seconds, then retry {count}/{retry_cnt}: \n", e)
+            time.sleep(sleep_sec)
+
+
+    response_chucks = []
+    if stream:
+        for chunk in response:
+            response_chucks.append(chunk)
+            content = chunk["choices"][0].get("delta", {}).get("content")
+            if content is not None:
+                if verbose:
+                    print(content, end='')
+    else:
+        content = response["choices"][0]['message']["content"]
+        # print(content)
+    print('\n\n')
+    # print("Got LLM reply.")
+        
+    response = response_chucks # good for saving
+    
+    return response
